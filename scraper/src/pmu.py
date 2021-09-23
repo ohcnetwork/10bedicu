6ede439bf5489278291a099cfab6f2f4c931ae39
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-09-24 02:08:03 +0530

    🧹 Clean PMU

--- scraper/src/pmu.py
+++ scraper/src/pmu.py
@@ -1,25 +1,28 @@
-import requests
-import csv
-from io import StringIO
-import json
+from config import PMUS_SHEET
+from shared import clean_value, get_data, dump_data
 
-root_dir = "data"
 
-def dump_data(filename, data):
-    with open(f"{root_dir}/{filename}", "w") as json_file:
-        json_file.write(json.dumps(data, sort_keys=True))
+def model_pmu(row):
+    # Transform a row into PMU object
+    # according to the schema in /schemas/pmu.json
+    # Make sure update /schemas/pmu.json while changing here
 
-csv_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQF7T3mxoUlKREnTLa4LXv6HHzWBe8UGz0gKp0t0mdaUqtc1jDNk5Fs6EjmuivzH0deMqDtW5WMYSWO/pub?gid=1517149316&single=true&output=csv'
-response = requests.get(csv_url)
-csv_data = csv.reader(StringIO(response.text))
-data = []
-for row in list(csv_data)[1:]:
-    if row[0].strip() != "":
-        data.append({
-            "state": row[0],
-            "name": row[1],
-            "description": row[2],
-            "image": row[3]
-        })
+    return {
+        "state": clean_value(row[0]),
+        "name": clean_value(row[1]),
+        "description": clean_value(row[2]),
+        "image": clean_value(row[3])
+    }
 
-dump_data("pmu.json", data)
+
+csv_data = get_data(PMUS_SHEET)
+
+json_data = []
+
+for row in csv_data:
+    pmu = model_pmu(row)
+    state = pmu["state"]
+    if state:
+        json_data.append(pmu)
+
+dump_data("pmu.json",  json_data)
