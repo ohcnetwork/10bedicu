a71cc991d5ba629f888d0f7ad9b388eaed48ff37
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-09-24 01:31:56 +0530

    🌎 Centralize the Common things

---
+++ scraper/src/config.py
@@ -0,0 +1,6 @@
+DONORS_SHEET = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQF7T3mxoUlKREnTLa4LXv6HHzWBe8UGz0gKp0t0mdaUqtc1jDNk5Fs6EjmuivzH0deMqDtW5WMYSWO/pub?gid=903745301&single=true&output=csv"
+HOSPITALS_SHEET = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQF7T3mxoUlKREnTLa4LXv6HHzWBe8UGz0gKp0t0mdaUqtc1jDNk5Fs6EjmuivzH0deMqDtW5WMYSWO/pub?gid=172030202&single=true&output=csv"
+PMUS_SHEET = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQF7T3mxoUlKREnTLa4LXv6HHzWBe8UGz0gKp0t0mdaUqtc1jDNk5Fs6EjmuivzH0deMqDtW5WMYSWO/pub?gid=1517149316&single=true&output=csv"
+META_SHEET = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQF7T3mxoUlKREnTLa4LXv6HHzWBe8UGz0gKp0t0mdaUqtc1jDNk5Fs6EjmuivzH0deMqDtW5WMYSWO/pub?gid=0&single=true&output=csv"
+
+ROOT_DIR = "data"
