0947c7654cb2cb7db2d8f543d0354b3c9b94746d
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-09-24 01:32:10 +0530

    🧹 Clean Donors

--- scraper/src/donors.py
+++ scraper/src/donors.py
@@ -1,25 +1,28 @@
-import requests
-import csv
-from io import StringIO
-import json
+from config import DONORS_SHEET
+from shared import clean_value, get_data, dump_data
 
-root_dir = "data"
 
-def dump_data(filename, data):
-    with open(f"{root_dir}/{filename}", "w") as json_file:
-        json_file.write(json.dumps(data, sort_keys=True))
+def model_donor(row):
+    # Transform a row into Donor object
+    # according to the schema in /schemas/donors.json
+    # Make sure update /schemas/donors.json while changing here
 
-csv_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQF7T3mxoUlKREnTLa4LXv6HHzWBe8UGz0gKp0t0mdaUqtc1jDNk5Fs6EjmuivzH0deMqDtW5WMYSWO/pub?gid=903745301&single=true&output=csv'
-response = requests.get(csv_url)
-csv_data = csv.reader(StringIO(response.text))
-data = {}
-for row in list(csv_data)[1:]:
-    if row[0].strip() != "":
-        data[row[0]] = {
-            "uuid": row[0],
-            "name": row[1],
-            "location": row[2],
-            "image": row[3]
-        }
+    return {
+        "uuid": clean_value(row[0]),
+        "name": clean_value(row[1]),
+        "location": clean_value(row[2]),
+        "image": clean_value(row[3]),
+    }
 
-dump_data("donors.json", data)
+
+csv_data = get_data(DONORS_SHEET)
+
+json_data = {}
+
+for row in csv_data:
+    donor = model_donor(row)
+    uuid = donor["uuid"]
+    if uuid:
+        json_data[uuid] = donor
+
+dump_data("donors.json", json_data)
