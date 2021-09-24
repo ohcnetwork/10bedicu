d77fd4d406be3cda5b08e97b91e17699f0d12f2b
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-09-24 12:36:18 +0530

    ✅ Complete Association

--- scraper/src/associate.py
+++ scraper/src/associate.py
@@ -1,8 +1,20 @@
-
-from shared import get_json_data
+from shared import dump_data, get_json_data
 
 # Association of Donors with Hospital
 hospital_data = get_json_data("hospitals.json")
 donors_data = get_json_data("donors.json")
 
-print(hospital_data[0])
+def associate_donor(hospital: dict):
+    donors = hospital.get("donors", [])
+    hospital.update({
+        "donors": [
+            donors_data[donor]
+            for donor in donors
+            if donor in donors_data
+        ]
+    })
+    return hospital
+
+hospital_data = list(map(associate_donor, hospital_data))
+
+dump_data("hospitals.json", hospital_data)
