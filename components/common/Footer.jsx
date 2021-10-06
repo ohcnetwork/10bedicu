bde7cec75e048a6281c29313a24698b83a464926
Khavin Shankar <khavinshankar@gmail.com>
2021-10-06 21:10:45 +0530

    ui: minor tweaks in footer (color change and padding alteration)

--- components/common/Footer.jsx
+++ components/common/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
         <footer className="flex flex-col items-center justify-center w-full border-t">
-            <div className="w-5/6 my-4 lg:grid grid-cols-2">
-                <div className="text-center lg:text-left">Copyright © 2021 10 Bed ICU - All Rights Reserved.</div>
+            <div className="w-5/6 my-4 items-center lg:grid grid-cols-2">
+                <div className="text-center text-sm text-gray-500 lg:text-left">Copyright © 2021 10 Bed ICU - All Rights Reserved.</div>
                 <div className="flex justify-center space-x-2 pt-2 lg:justify-start lg:pt-0">
                     <div>facebook</div>
                     <div>twitter</div>
@@ -10,14 +10,14 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className="my-2 mb-6">
-                <ul className="flex justify-center space-x-2">
-                    <li><a href="https://10bedicu.org/">Home</a></li>
-                    <li><a href="https://10bedicu.org/tech-platform">Tech Platform</a></li>
-                    <li><a href="https://10bedicu.org/media">Media</a></li>
-                    <li><a href="https://10bedicu.org/donate-now">Donate Now</a></li>
-                    <li><a href="https://10bedicu.org/deployment">Deployment</a></li>
-                    <li><a href="https://10bedicu.org/our-team">Our Team</a></li>    
+            <div className="my-2 mb-6 lg:mt-2">
+                <ul className="flex justify-center space-x-4 text-gray-600">
+                    <li className="hover:text-blue-500"><a href="https://10bedicu.org/">Home</a></li>
+                    <li className="hover:text-blue-500"><a href="https://10bedicu.org/tech-platform">Tech Platform</a></li>
+                    <li className="hover:text-blue-500"><a href="https://10bedicu.org/media">Media</a></li>
+                    <li className="hover:text-blue-500"><a href="https://10bedicu.org/donate-now">Donate Now</a></li>
+                    <li className="hover:text-blue-500"><a href="https://10bedicu.org/deployment">Deployment</a></li>
+                    <li className="hover:text-blue-500"><a href="https://10bedicu.org/our-team">Our Team</a></li>    
                 </ul>
             </div>
         </footer>
