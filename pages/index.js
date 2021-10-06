8c4ec0cbdc00790353b92f1552f9535739cc6bcc
Shailesh Aanand <anaandshailu@gmail.com>
2021-10-06 22:09:05 +0530

    Replicate 10bedicu header

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,70 @@
 import HomeStatesList from "@components/HomeStatesList";
 
 const HomePage = () => {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <main className="flex flex-col items-center justify-center w-full flex-1">
-        <div className="p-3 text-center">
-          <img src="/10bedlogo.png" className="h-20" />
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen pb-2">
+            <main className="flex flex-col items-center justify-center w-full flex-1">
+                <div className="text-center w-full bg-[#008390]">
+                    <div className="max-w-screen-xl p-3 mx-auto flex justify-between">
+                        <a href="/">
+                            <img src="/10bedlogo.png" className="h-20" />
+                        </a>
+                        <ul className="flex justify-center items-center">
+                            <li>
+                                <a
+                                    className="text-white mx-6 text-xl border-b-2 border-transparent hover:border-white"
+                                    href="/"
+                                >
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    className="text-white mx-6 text-xl border-b-2 border-transparent hover:border-white"
+                                    href="#"
+                                >
+                                    Program States
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    className="text-white mx-6 text-xl border-b-2 border-transparent hover:border-white"
+                                    href="#"
+                                >
+                                    Tech Platform
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    className="text-white mx-6 text-xl border-b-2 border-transparent hover:border-white"
+                                    href="#"
+                                >
+                                    Media
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    className="text-white mx-6 text-xl border-b-2 border-transparent hover:border-white"
+                                    href="#"
+                                >
+                                    Donate Now
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    className="text-white mx-6 text-xl border-b-2 border-transparent hover:border-white"
+                                    href="#"
+                                >
+                                    Deployment
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <HomeStatesList />
+            </main>
         </div>
-        <HomeStatesList />
-      </main>
-    </div>
-  );
-}
+    );
+};
 
 export default HomePage;
