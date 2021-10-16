f81c749ad25afb54788a7f9ef78f4614eca96ad0
Gigin George <mail.gigin@gmail.com>
2021-10-16 16:31:23 +0530

    Update Status Scraper

--- scraper/src/status.py
+++ scraper/src/status.py
@@ -23,20 +23,20 @@ def model_hospital_status(state, row):
         "pincode": clean_value(row[4]),
         "hospital_name": clean_value(row[1]),
         "hospital_type": clean_value(row[3]),
-        "site_space": clean_value(clean_value(row[31])),
-        "site_electrical": clean_value(clean_value(row[36])),
-        "site_internet": clean_value(clean_value(row[38])),
-        "site_oxygen": clean_value(clean_value(row[37])),
-        "site_ready": clean_value(clean_value(row[45])),
-        "equipment_ordered": clean_value(clean_value(row[48])),
-        "equipment_delivered": clean_value(clean_value(row[49])),
-        "equipment_installed": clean_value(clean_value(row[51])),
-        "equipment_staff_trained": clean_value(clean_value(row[53])),
-        "tech_trained": clean_value(clean_value(row[58])),
-        "tech_hospital_registration": clean_value(clean_value(row[60])),
-        "tech_patient_management": clean_value(clean_value(row[61])),
-        "tech_tele_icu_live": clean_value(clean_value(row[62])),
-        "icu_live": clean_value(clean_value(row[64])),
+        "site_space": clean_value(clean_value(row[36])),
+        "site_electrical": clean_value(clean_value(row[42])),
+        "site_internet": clean_value(clean_value(row[44])),
+        "site_oxygen": clean_value(clean_value(row[46])),
+        "site_ready": clean_value(clean_value(row[54])),
+        "equipment_ordered": clean_value(clean_value(row[57])),
+        "equipment_delivered": clean_value(clean_value(row[58])),
+        "equipment_installed": clean_value(clean_value(row[60])),
+        "equipment_staff_trained": clean_value(clean_value(row[62])),
+        "tech_trained": clean_value(clean_value(row[67])),
+        "tech_hospital_registration": clean_value(clean_value(row[69])),
+        "tech_patient_management": clean_value(clean_value(row[70])),
+        "tech_tele_icu_live": clean_value(clean_value(row[71])),
+        "icu_live": clean_value(clean_value(row[73])),
     }
 
 sheet_data = get_data(STATES_SHEET)
@@ -46,8 +46,7 @@ for sheet in sheet_data:
     print(model_state(sheet)["sheet"])
     csv_data = get_data_header_status(model_state(sheet).get("sheet"))
     for row in csv_data:
-        # Checking whether it's live
-        if clean_value(row[0]):
+        if (clean_value(row[0]) and clean_value(row[1]) and len(row) >= 74):
             hospital = model_hospital_status(sheet,row)
             json_data.append(hospital)
 
